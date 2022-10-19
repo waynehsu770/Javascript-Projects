43a37e66ed66d1162496dba9a43762588173b212
waynehsu770 <waynehsu770@gmail.com>
2022-10-19 15:23:06 +0800

    made project4 dictionaries practice files

---
+++ Basic-Javascript-Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function KeyValues() {
+    var Car = {
+        Brand: "Mercedes",
+        Color: 'Blue',
+        Class: 'E350',
+        Style: 'Coupe',
+        Year: 2020
+    }
+    document.getElementById('Dictionary').innerHTML = Car.Class;
+}
