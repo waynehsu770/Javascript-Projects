cfe16d0c0ca0588b3b1a03d2911962111a259c57
waynehsu770 <waynehsu770@gmail.com>
2022-10-19 15:11:02 +0800

    added notes to main.js

--- Basic-Javascript-Projects/Project3_math_operators/JS/main.js
+++ Basic-Javascript-Projects/Project3_math_operators/JS/main.js
@@ -19,23 +19,23 @@ function Division() {
 }
 
 function Remainder() {
-    var E = 10 % 3;
+    var E = 10 % 3; // This gets the remainder from 10 divided by 3 which is 1
     document.getElementById("Remain").innerHTML = 'The remainder of 10 / 3 is: ' + E;
 }
 
 function IncrementUp() {
     var F = 3;
-    F++;
+    F++; // This adds 1 to the assigned value for variable F
     document.getElementById('Up').innerHTML = '3 incremented up is: ' + F;
 }
 
 function IncrementDown() {
     var G = 6;
-    G--;
+    G--; // This subtracts 1 from the assigned value for variable G
     document.getElementById('Down').innerHTML = '6 incremented down is: ' + G;
 }
 
 function Random() {
-    var Ran = Math.round(Math.random()*100);
+    var Ran = Math.round(Math.random()*100); // This generates a random whole number between 0 and 100
     document.getElementById('Math').innerHTML = 'The random number between 0 and 100 is: ' + Ran;
 }
