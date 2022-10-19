8267085a25686ad33786ed21496fe12cc0543fab
waynehsu770 <waynehsu770@gmail.com>
2022-10-19 12:53:07 +0800

    created project 2 folder

---
+++ Basic-Javascript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function First() {
+    var str = 'This is the button text!';
+    str += ' This is the end!'; // This concatenates the 2 strings thru the += operator
+    document.getElementById('Button_Text').innerHTML = str; 
+}
