7ce5636f8ee95dfce47b0df24bdd22ef3355ae1c
waynehsu770 <waynehsu770@gmail.com>
2022-10-19 11:18:29 +0800

    Created index and main for javascript practice

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var A = 'Hello guest!';
+alert(A);  // This creates a pop up alert that says Hello guest! 
+
+var Sent1 = 'This is the beginning of the string';
+var Sent2 = ' and this is the end of the string.';
+document.write(Sent1 + Sent2); // This prints the variables Sent1 and Sent2 concatenated together
+
+document.write("<br>")
+var B = 3 + 2 // Statement declaring the variable B equal to the expression of 3 + 2
+document.write(B);
