b01168f114b209936b9ab7df9467c2b45d531cd3
waynehsu770 <waynehsu770@gmail.com>
2022-10-20 10:00:54 +0800

    updated main.js

--- Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -10,8 +10,8 @@ document.write(2 > 1);
 document.write("<br>");
 document.write( 10 < 2);
 document.write("<br>");
-document.write( 20 > 2 && 3 < 5);
+document.write( 20 > 2 && 3 < 5); // And operator, if both expressions are true, it returns true
 document.write("<br>");
-document.write(2 > 5 || 10 > 2);
+document.write(2 > 5 || 10 > 2); // Or operator, if both or one of the expressions is true, it returns true
 document.write("<br>");
-document.write(!(2 > 10));
+document.write(!(2 > 10)); // ! evaluates whether the expression is true or false. If it is false, it returns true
